71ae68dc2e472e458f56d9898832517abd7f9e69
Kifaa26 <akifahpalekar@gmail.com>
2024-05-14 14:10:02 +0200

    first_commit

---
+++ script/code.js
@@ -0,0 +1,13 @@
+let firstName = "Aakifah";
+let lastName = "Palekar";
+
+console.log(firstName + " " + lastName)
+
+let numberVar = 5;
+
+console.log(numberVar*5);
+
+const myName = "Aakifah";
+myName = "Angel"
+
+// Got an error 
